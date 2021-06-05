c94cb4f4e4bd8e6bd0735020dfc3b3d43670c1e9
JoeScho <joeschofield@live.co.uk>
2021-06-05 11:30:38 +0100

    colours and icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { faGithub, faLinkedin, faMedium } from '@fortawesome/free-brands-svg-icons';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   Flex,
-  Link,
+  Box,
   Switch,
   IconButton,
   useColorMode,
@@ -17,15 +18,37 @@ const Footer = () => {
   return (
     <Flex height="10vh" alignItems="center" justifyContent="center" bgColor={footerBackground}>
       <a href="https://www.linkedin.com/in/joescho/" target="_blank" >
-        <IconButton icon={<FontAwesomeIcon icon={faLinkedin} size="3x" />} />
+        <IconButton
+          icon={<FontAwesomeIcon icon={faLinkedin} size="3x" />}
+          backgroundColor={footerBackground}
+        />
       </a>
       <a href="https://github.com/JoeScho" target="_blank" style={{ margin: 10 }} >
-        <IconButton icon={<FontAwesomeIcon icon={faGithub} size="3x" />} />
+        <IconButton
+          icon={<FontAwesomeIcon icon={faGithub} size="3x" />}
+          backgroundColor={footerBackground}
+        />
       </a>
       <a href="https://medium.com/@joescho" target="_blank" >
-        <IconButton icon={<FontAwesomeIcon icon={faMedium} size="3x" />} />
+        <IconButton
+          icon={<FontAwesomeIcon icon={faMedium} size="3x" />}
+          backgroundColor={footerBackground}
+        />
       </a>
-      <Switch position='absolute' right='5%' onChange={toggleColorMode} checked={colorMode === 'light'} />
+      <Box position='absolute' right='2%'>
+        <IconButton
+          icon={<FontAwesomeIcon icon={faMoon} />}
+          backgroundColor={footerBackground}
+        />
+        <Switch
+          onChange={toggleColorMode}
+          checked={colorMode === 'light'}
+        />
+        <IconButton
+          icon={<FontAwesomeIcon icon={faSun} />}
+          backgroundColor={footerBackground}
+        />
+      </Box>
     </Flex>
   );
 };
--- src/components/HeadingWithBackButton.js
+++ src/components/HeadingWithBackButton.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { faBackward } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Heading, IconButton } from '@chakra-ui/react';
+import { Heading, IconButton, useColorModeValue } from '@chakra-ui/react';
 
-const HeadingWithBackButton = ({ title }) => (
-  <Heading mb='60px'>
-    <a href="/" style={{ marginRight: 20 }}>
-      <IconButton icon={<FontAwesomeIcon icon={faBackward} />} />
-    </a>
-    {title}
-  </Heading>
-);
+const HeadingWithBackButton = ({ title }) => {
+  const buttonBackground = useColorModeValue('gray.100', 'gray.700');
+
+  return (
+    <Heading mb='60px'>
+      <a href="/" style={{ marginRight: 20 }}>
+        <IconButton
+          icon={<FontAwesomeIcon icon={faBackward} />}
+          backgroundColor={buttonBackground}
+        />
+      </a>
+      {title}
+    </Heading>
+  );
+};
 
 export default HeadingWithBackButton;
